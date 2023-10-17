a14af86d9cc676675fde11f87e6d15338d71acf1
Rayanmoh584 <rayanmohammed584@gmail.com>
2023-10-17 21:12:43 +0300

    edit1

--- main.h
+++ main.h
@@ -1,14 +1,23 @@
-#include <stdio.h>                                         
-/**
- *main - Entry point
- *                                                          *Description : A C program
- *
- *Return: Always 0 (success)
-*/
+#ifndef __HEADER__
+#define __HEADER__
 
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of
-art,\n");
-return (0);
-}
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+char *put_char_in_final(char *final_str, char ch, unsigned int *str_index,
+		unsigned int *count);
+char *put_string_in_final(char *final_str, char *str, unsigned int *str_index,
+		unsigned int *count);
+
+int _strlen(char *str, unsigned int *count);
+int check_null(const char *format);
+int _strlen2(char *str);
+char *printing_nums(char *final_str, unsigned int *str_index,
+		unsigned int *count, int num);
+char *positive_handling(int num);
+char *minus_handling(int num);
+
+#endif /* __HEADER__ */
+
